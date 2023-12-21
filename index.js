70abfdf03417f9066279b12340eda327be4fe436
Kiprotich-Amos <kiprotichamoh11@gmail.com>
2023-12-22 02:30:13 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
-function scuberGreetingForFeet(){
+var mambet;
+var gone;
+var country;
+var code;
+var city;
+var ride;
+var tip;
+var tipReturn;
+
+var chemoget;
+var bye;
+
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  if(ride <= 400){
+    console.log()
+    return 'This one is on me!';
+  }else if (ride > 2000 && ride < 2500){
+    mambet = 'I will gladly take your thirty bucks.';
+    console.log(mambet);
+    return mambet;
+  }else if (ride > 2500 ){
+    gone = 'No can do.';
+    return gone;
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if(city === 'NYC'){
+
+    country = 'Ok, sounds good.';
+    return country;
+  }else{
+    code = 'No go.';
+    return code; 
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+
+  if (tip == 'generous'){
+    tipReturn = 'Thank you so much.';
+    return tipReturn;
+  }else if(tip == 'not as generous'){
+    chemoget = 'Thank you.';
+    return chemoget;
+  }else{
+    bye = 'Bye.';
+    return bye;
+  }
 }
